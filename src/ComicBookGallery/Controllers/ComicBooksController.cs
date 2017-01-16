5617fcf94ada1bb8767fa60154c18443ae3a0f02
melaniesdavis <melaniesdavis@hotmail.com>
2017-01-16 15:36:06 -0500

    Added Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -7,8 +7,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    class ComicBooksController : Controller
+    public class ComicBooksController : Controller
     {
-
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
